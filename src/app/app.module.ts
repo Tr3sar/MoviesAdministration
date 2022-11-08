f46b1477f3c0ff6e7e9c6b50e1ead590f173adc6
tr3sar <josepcdg@gmail.com>
2022-11-08 20:20:17 +0100

    Afegir el logo en la app i utilitzar Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MovieDescriptionComponent } from './components/movie-description/movie-description.component';
+import { MovieListComponent } from './components/movie-list/movie-list.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieDescriptionComponent,
+    MovieListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
