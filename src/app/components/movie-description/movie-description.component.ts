9c8cdf1f4a390e8b8991f8de97e329da16ab9092
tr3sar <josepcdg@gmail.com>
2022-11-09 12:04:08 +0100

    Descripció del component feta

--- src/app/components/movie-description/movie-description.component.ts
+++ src/app/components/movie-description/movie-description.component.ts
@@ -1,13 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Movie } from 'src/app/interfaces/movies';
+import { MoviesService } from 'src/app/services/movies.service';
 
 @Component({
   selector: 'app-movie-description',
   templateUrl: './movie-description.component.html',
   styleUrls: ['./movie-description.component.scss']
 })
-export class MovieDescriptionComponent implements OnInit {
+export class MovieDescriptionComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  @Input() movieId: string = '';
+  movieSelected!: Movie;
+
+  constructor(private moviesService: MoviesService) { }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.movieId !== '') {
+      console.log('ngOnChanges movies description');
+      this.moviesService.getMovieById(this.movieId).subscribe((movie) => {
+        console.log(movie)
+        this.movieSelected = movie;
+      });
+    }
+  }
 
   ngOnInit(): void {
   }
