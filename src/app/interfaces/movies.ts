8ed80a47da26ebb39cad2e23ea81c6d4c8329604
tr3sar <josepcdg@gmail.com>
2022-11-09 12:04:59 +0100

    Obtindre una pelicula amb l'id

--- src/app/interfaces/movies.ts
+++ src/app/interfaces/movies.ts
@@ -1,4 +1,4 @@
-interface RootObject {
+export interface RootObject {
   page: number;
   results: Result[];
   total_pages: number;
@@ -20,4 +20,55 @@ interface Result {
   video: boolean;
   vote_average: number;
   vote_count: number;
+}
+
+export interface Movie {
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection?: any;
+  budget: number;
+  genres: Genre[];
+  homepage: string;
+  id: number;
+  imdb_id: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: Productioncompany[];
+  production_countries: Productioncountry[];
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages: Spokenlanguage[];
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+interface Spokenlanguage {
+  english_name: string;
+  iso_639_1: string;
+  name: string;
+}
+
+interface Productioncountry {
+  iso_3166_1: string;
+  name: string;
+}
+
+interface Productioncompany {
+  id: number;
+  logo_path?: any;
+  name: string;
+  origin_country: string;
+}
+
+interface Genre {
+  id: number;
+  name: string;
 }
--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs';
+import { Movie, RootObject } from '../interfaces/movies'
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +11,19 @@ export class MoviesService {
   baseUrl = 'https://api.themoviedb.org/3/movie/'
   apiKey = '9de91dffec28830cdaaf7ecc3225502e'
 
-  constructor(private http: HttpClient) { }
+  movies: any;
+
+  constructor(private http: HttpClient) { 
+    this.getPopularMovies().subscribe((movies) => {
+      this.movies = Object.values(movies)[1];
+    })
+  }
 
   getPopularMovies() : Observable<RootObject[]>{
     return this.http.get<RootObject[]>(this.baseUrl + 'popular?api_key=' + this.apiKey + '&language=es-ES&page=1');
   }
+
+  getMovieById(id: string) {
+    return this.http.get<Movie>(this.baseUrl + '/' + id + '?api_key=' + this.apiKey + '&language=es-ES');
+  } 
 }
