74d43a082f0a4cbb9d872ab29a0e8d66640d5271
tr3sar <josepcdg@gmail.com>
2022-11-09 12:03:18 +0100

    Actualització moduls angular material i httpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,14 @@ import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card'
+
 import { MovieDescriptionComponent } from './components/movie-description/movie-description.component';
 import { MovieListComponent } from './components/movie-list/movie-list.component';
+import { MoviesService } from './services/movies.service';
+
+
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -19,9 +25,11 @@ import { MovieListComponent } from './components/movie-list/movie-list.component
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    HttpClientModule,
+    MatCardModule
   ],
-  providers: [],
+  providers: [MoviesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
